960c2059c338530ed709b7b39c6002f9f7e63896
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-05-20 16:51:53 +0200

    started THE NEXT JS Project initial Commit

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <h1>Hello About The Wild Oasis</h1>;
+}
+
+export default Page;
---
+++ app/account/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <h1>Account of Wild Oasis</h1>;
+}
+
+export default Page;
