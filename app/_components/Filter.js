8c71fb1c81b395a655ab3083bc7174b0f83aabd8
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-08-16 22:18:51 +0200

    added Loading spinner to Cabins page

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -9,6 +9,8 @@ function Filter() {
   const router = useRouter();
   const pathname = usePathname();
 
+  const activeFilter = searchParms.get("capacity") ?? "all";
+
   function handleFilter(filter) {
     const params = new URLSearchParams(searchParms);
 
@@ -18,31 +20,49 @@ function Filter() {
 
   return (
     <div className="flex border border-primary-800">
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("all")}
+      <Button
+        filter="all"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         All cabins
-      </button>
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("small")}
+      </Button>
+      <Button
+        filter="small"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
-        1 &mdash;3 Guests
-      </button>
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("medium")}
+        1&mdash;2 Guests
+      </Button>
+      <Button
+        filter="medium"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
-        4 &mdash;7 Guests
-      </button>
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("large")}
+        3&mdash;7 Guests
+      </Button>
+      <Button
+        filter="large"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         8&mdash;12 Guests
-      </button>
+      </Button>
     </div>
+  );
+  s;
+}
+
+function Button({ filter, handleFilter, activeFilter, children }) {
+  return (
+    <button
+      className={`px-5 py-2 hover:bg-primary-700 ${
+        filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+      } `}
+      onClick={() => handleFilter(filter)}
+    >
+      {children}
+    </button>
   );
 }
 
